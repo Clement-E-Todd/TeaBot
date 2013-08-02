ccd35761b941776ff4636d7d7285fe520b62c111
Clement Todd <clement@arcticempire.ca>
2013-08-02 18:50:52 -0400

    Commented the jump input

--- Scripts/Actions/TBActionJumpInput.cs
+++ Scripts/Actions/TBActionJumpInput.cs
@@ -1,6 +1,20 @@
 using UnityEngine;
 
 public interface TBActionJumpInput {
+	
+	/// <summary>
+	/// Starting Jump
+	/// </summary>
+	/// <returns>
+	/// A bool reflecting whether the character wants to start a new jump.
+	/// </returns>
 	bool StartingJump();
+	
+	/// <summary>
+	/// Holding Jump
+	/// </summary>
+	/// <returns>
+	/// A bool reflecting whether the character intends to continue the jump for now.
+	/// </returns>
 	bool HoldingJump();
 }
